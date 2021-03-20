593a9b77d046cdf641935e021c2299118b216bd8
JellyLu <634725546@qq.com>
2021-03-20 12:39:30 +0800

    [Yangjie] flexible layout example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          color: Colors.white,
+          height: 100,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textDirection: TextDirection.ltr,
+            children: [
+              Container(color: Colors.green, width: 100, height: 80),
+              Spacer(flex: 2),
+              Container(width: 30, height: 60, color: Colors.red),
+              Spacer(flex: 1),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
