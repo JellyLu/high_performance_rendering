2e14464eff8a56f722ffa01e8101594a8fe1e86f
JellyLu <634725546@qq.com>
2021-03-20 14:45:17 +0800

    [Yangjie] three trees example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Container());
+  runApp(
+    MaterialApp(home: MyApp()),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  bool _switchTree;
+
+  Widget get firstWidget => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Hello World'),
+          Icon(Icons.favorite, color: Colors.red),
+        ],
+      );
+  Widget get secondWidget => Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Hello Chengdu'),
+          Icon(Icons.favorite_border, color: Colors.red),
+        ],
+      );
+
+  Widget get switchButton => TextButton(
+        child: Text('Switch tree'),
+        onPressed: () {
+          setState(() {
+            _switchTree = !_switchTree;
+          });
+        },
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    _switchTree = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [_switchTree ? firstWidget : secondWidget, switchButton],
+    );
+  }
 }
