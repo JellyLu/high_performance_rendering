d082484262474c048bdd8526e82f349d0e0cb077
Sasa Cheng <sscheng@thoughtworks.com>
2021-04-24 12:36:36 +0800

    udpate example to make hot-reload work

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
       child: RichText(
         text: TextSpan(text: 'Hello World'),
         textDirection: TextDirection.ltr,
       ),
-    ),
-  );
+    );
+  }
 }
